53127da3df464460f18b6275c97f105a9a68395f
SvetlaGeorgieva <sv.georgieva@gmail.com>
2014-03-18 21:42:52 +0200

    adding list methods - add_mail, and tetsts

--- list.py
+++ list.py
@@ -3,10 +3,17 @@ class List():
     def __init__(self, index, name):
         self.index = index
         self.name = name
-        
+        self.emails = []
 
     def get_list_name(self):
         return self.name
 
     def get_list_index(self):
         return self.index
+
+    def add_email(self, name, email):
+        self.emails.append((name, email))
+        return self.emails
+
+    def show_list(self, index):
+        pass
--- list_tests.py
+++ list_tests.py
@@ -12,6 +12,25 @@ class List_tests(unittest.TestCase):
         list1 = list.List(1, "List1")
         self.assertEqual(1, list1.get_list_index())
 
+    def test_add_email(self):
+        list1 = list.List(1, "List1")
+        self.assertEqual([("Ivo", "ivo@mail.bg")], list1.add_email("Ivo", "ivo@mail.bg"))
+        self.assertEqual([("Ivo", "ivo@mail.bg"), ("Pesho", "pesho@mail.bg")], list1.add_email("Pesho", "pesho@mail.bg"))
+
+    def test_show_list(self):
+        list1 = list.List(1, "List1")
+        result1 = ""
+        self.assertEqual(result1, list1.show_list(1))
+
+        list1.add_email("Ivo", "ivo@mail.bg")
+        result2 = "[1] Ivo - ivo@mail.bg"
+        self.assertEqual(result2, list1.show_list(1))
+
+        list1.add_email("Pesho", "pesho@mail.bg")
+        result2 = "[1] Ivo - ivo@mail.bg\n[2] Pesho - pesho@mail.bg"
+        self.assertEqual(result2, list1.show_list(1))
+
+
 
 
 if __name__ == '__main__':
