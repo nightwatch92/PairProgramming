b9f455a73aeede8e2df4ae25d7fbfe7292526fa0
SvetlaGeorgieva <sv.georgieva@gmail.com>
2014-03-22 16:12:27 +0200

    converter finished

--- converter.py
+++ converter.py
@@ -9,22 +9,33 @@ class Converter():
 
     def get_file_name(self):
         file_name = self.mailList.get_list_name().replace(" ", "_")
+        file_name += ".json"
         return file_name
 
-# will be changed in order to look like this:
-#[list_name, {name: name1, email: email1}, {name: name2, email: email2}, ..]
-    def convert_to_dict(self):
+
+    def convert_to_dict_list(self):
         self.emails = self.mailList.get_list_emails()
-        self.emails_dict = {}
+        self.emails_list = []
 
+        #creating a dictionary from every (name, email) and adding it to a list
         for i in range (len(self.emails)):
-            self.emails_dict[self.emails[i][0]] = self.emails[i][1]
-        return self.emails_dict
+            email_dict = {}
+            email_dict["name"] = self.emails[i][0]
+            email_dict["email"] = self.emails[i][1]
+
+            self.emails_list.append(email_dict)
+        return self.emails_list
 
 
-
+    #list_name, [{name: name1, email: email1}, {name: name2, email: email2}]
     def save_prepare(self):
-        content = ""
-        self.emails_dict = self.convert_to_dict()
-        content = json.dumps([self.mailList_name, self.emails_dict], indent=4, sort_keys = True, separators=(',', ':'))
+        content = self.mailList_name
+        self.emails_dict = self.convert_to_dict_list()
+        content += "\n" + json.dumps(self.emails_dict, indent=4, sort_keys = True, separators=(',', ':'))
         return content
+
+    def save(self):
+        file_to_save = open(self.get_file_name(), "w")
+        content = self.save_prepare()
+        file_to_save.write(content)
+        file_to_save.close()
--- converter_tests.py
+++ converter_tests.py
@@ -1,6 +1,7 @@
 import converter
 import mailList
 import unittest
+import os
 
 class Converter_tests(unittest.TestCase):
     """docstring for Converter_tests"""
@@ -24,20 +25,35 @@ class Converter_tests(unittest.TestCase):
 
 
     def test_get_file_name(self):
-        self.assertEqual("List_1", self.conv1.get_file_name())
-        self.assertEqual("Hack_Bulgaria", self.conv2.get_file_name())
+        self.assertEqual("List_1.json", self.conv1.get_file_name())
+        self.assertEqual("Hack_Bulgaria.json", self.conv2.get_file_name())
 
-    def test_convert_to_dict(self):
-        d = {"Ivo": "ivo@mail.bg", "Pesho": "pesho@mail.bg"}
-        self.assertEqual(d, self.conv1.convert_to_dict())
+    def test_convert_to_dict_list(self):
+        d = [{"name": "Ivo", "email": "ivo@mail.bg"}, {"name": "Pesho", "email": "pesho@mail.bg"}]
+        self.assertEqual(d, self.conv1.convert_to_dict_list())
 
+    """ Written just to see the result, the test itself is not correct
     def test_save_prepare(self):
-        result = "{}"
+        result = "[]"
         self.assertEqual(result, self.conv1.save_prepare())
+    """
 
-        #result = "{\'name\': \'Ivo\', \'email\': \'ivo@mail.bg\'}"
-        #self.assertEqual(result, self.conv3.save_prepare())
-        
+    def test_save_name(self):
+        self.conv1.save()
+        file_name = self.conv1.get_file_name()
+
+        file = open(file_name, "r")
+        contents = file.read()
+        file.close()
+
+        lines = contents.split("\n")
+        self.assertEqual("List 1", lines[0])
+        os.remove(file_name)
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
